5efd8c53147c867fb9eae68f7842e1e5eae7cf36
Yunan Luo <luoyunan@gmail.com>
2019-03-27 11:37:48 -0500

    fix filename

--- src/run_DCA.m
+++ src/run_DCA.m
@@ -5,7 +5,7 @@ dim_drug = 100;
 dim_prot = 400;
 
 drugNets = {'Sim_mat_drug_drug', 'Sim_mat_drug_disease', 'Sim_mat_drug_se', 'Sim_mat_Drugs'};
-proteinNets = {'Sim_protein_protein', 'Sim_mat_protein_disease', 'Sim_mat_Proteins'};
+proteinNets = {'Sim_mat_protein_protein', 'Sim_mat_protein_disease', 'Sim_mat_Proteins'};
 
 tic
 X = DCA(drugNets, dim_drug, restartProb, maxiter);
